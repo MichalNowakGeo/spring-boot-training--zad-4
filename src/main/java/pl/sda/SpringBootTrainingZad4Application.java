958ccb3f5c1aed65a2ac725558bcd1633d9ad965
Michal Nowak <michal.nowak.geo@gmail.com>
2023-02-26 16:36:26 +0100

    first training spring boot web

---
+++ src/main/java/pl/sda/SpringBootTrainingZad4Application.java
@@ -0,0 +1,13 @@
+package pl.sda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrainingZad4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrainingZad4Application.class, args);
+	}
+
+}
